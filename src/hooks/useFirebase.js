e4e3dc5bb5f3511286ae44344fbaa788b64efb03
muntasir-i01 <you@example.communtasirm55@gmail.com>
2022-08-18 11:17:39 +0600

    google pop up sign in works but not change login state /Header.js

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../firebase.init';
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
+    
     const [user, setUser] = useState({});
 
     useEffect(() => {
@@ -8,7 +14,15 @@ const useFirebase = () => {
     }, [])
 
     const signInWithGoogle = () => {
-        console.log('google sign in');
+        signInWithPopup(auth, googleProvider)
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+        
     }
     // return [user, setUser];
     return { user, signInWithGoogle };
